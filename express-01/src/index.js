faa4c6abdbfdf4edf6541dea051dc356a8dbf96d
Marcio Bueno <marciobueno@gmail.com>
2023-09-15 22:33:14 +0000

    alterações nas rotas para realizar as consultas no BD

--- express-01/src/index.js
+++ express-01/src/index.js
@@ -14,10 +14,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use((req, res, next) => {
+app.use(async (req, res, next) => {
   req.context = {
     models,
-    me: models.users[1],
+    me: await models.User.findByLogin("rwieruch"),
   };
   next();
 });
